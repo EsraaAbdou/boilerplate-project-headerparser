75639fcb882f60d268261710bc0636fa376c4f44
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-15 22:51:52 +0000

    remove nodemon

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get("/api/whoami", function (req, res) {
   res.json({ipaddress : headers['x-forwarded-for'], language: headers['accept-language'] , software: headers['user-agent']});
 });
 
-// listen for requests :)
-// var listener = app.listen(process.env.PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
+listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
 
-app.listen(3000)
+// app.listen(3000)
